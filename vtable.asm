abe4bb24a877711d27893925b284e8613236f733
Peter Heinrich <peter.heinrich@gmail.com>
2007-11-12 21:35:27 +0000

    Modified VTable.dispatch() to handle 16-bit key values

--- vtable.asm
+++ vtable.asm
@@ -21,7 +21,7 @@
    global   VTable.dispatch
 
    ; Dependencies
-   extern   Util.Save
+   extern   Util.Frame
 
 
 
@@ -30,13 +30,13 @@
 ;; ---------------------------------------------------------------------------
 
 ;; ----------------------------------------------
-;;  VTable.dispatch( WREG funcKey, TBLPTR vtable )
+;;  VTable.dispatch( frame[0..1] funcKey, TBLPTR vtable )
 ;;
 ;;  Iterates over a list of key-value entries stored in program memory,
 ;;  searching for a function pointer that matches the key specified.  Each
-;;  entry in the key-value list starts with an 8-bit identifier sign-extended
-;;  into 16 bits, followed by the lower 16 bits of a program memory address in
-;;  little-endian format (the upper 5 bits are always treated as 0).
+;;  entry in the key-value list starts with a 16-bit identifier, followed by
+;;  the lower 16 bits of a program memory address in little-endian format (the
+;;  upper 5 bits are always treated as 0).
 ;;
 ;;  This method searches linearly through the list until a matching id or -1
 ;;  is found.  The ids don't have to be in any particular numerical order, al-
@@ -61,7 +61,6 @@
 ;;
 VTable.dispatch:
    ; Back up the pointer two bytes to account for the first iteration.
-   movwf    Util.Save               ; preserve the id
    tblrd*-
    tblrd*-
 
@@ -69,12 +68,19 @@ lookup:
    ; Find the correct function pointer, based on the id specified.
    tblrd*+                          ; skip the function pointer from the last entry
    tblrd*+
+
    tblrd*+                          ; read the low byte of the id
-   movf     TABLAT, W               ; retrieve it from the table latch
-   tblrd*+                          ; skip the id's high byte
-   bn       vecJump                 ; if -1, we didn't find a match
-   cpfseq   Util.Save               ; is this the function requested?
-     bra    lookup                  ; no, advance to the next entry
+   movff    TABLAT, Util.Frame + 2
+   tblrd*+                          ; read the high byte of the id
+   movff    TABLAT, Util.Frame + 3
+
+   ; Compare the two-byte id to the first parameter.
+   movf     Util.Frame + 0, W
+   cpfseq   Util.Frame + 2
+     bra    noMatch
+   movf     Util.Frame + 1, W
+   cpfseq   Util.Frame + 3
+     bra    noMatch
 
 vecJump:
    ; Dispatch to the correct method.  Push the current PC, then replace the pushed
@@ -86,8 +92,15 @@ vecJump:
    tblrd*+
    movf     TABLAT, W               ; can't movff to TOSH, either
    movwf    TOSH
-   movf     Util.Save, W            ; callee gets the same WREG parameter we did
    return
+
+noMatch:
+   movf     Util.Frame + 2, W
+   cpfseq   Util.Frame + 3          ; are both id bytes equal?
+     bra    lookup                  ; no, can't be -1 (0xffff)
+   comf     WREG, F                 ; yes, complement one of them
+   bz       vecJump                 ; 0 => id = -1, so we're done
+   bra      lookup                  ; otherwise, keep looking
 
 
 
