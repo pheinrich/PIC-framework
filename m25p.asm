82cecab90e34471ebb9a0f3535e9a0072b5709a7
Peter Heinrich <peter.heinrich@gmail.com>
2008-04-28 05:06:03 +0000

    Renamed m25p10.asm to m25p.asm to reflect more general applicability; Rewrote all methods to eliminate calls to SPI.ioByte() and SPI.ioWord() (for consistency); Fixed M25P.powerUp() to push dummy address, in order to retrieve 1-byte electronic signature; Rolled waitForWriteComplete() into endCommandConfirmWrite(); Added (temporary) busy wait to endCommandConfirmWrite() between L/H/L transition on CS# pin; Added extra SPI.io() call to inline status read command, via beginCommand()

--- m25p.asm
+++ m25p.asm
@@ -8,7 +8,7 @@
 ;;  $URL$
 ;;  $Revision$
 ;;
-;;  Provides a basic wrapper to control the M25P10-A serial flash memory chip.
+;;  Provides a basic wrapper to control the M25P-type serial flash memories.
 ;;  This is a low-voltage 1-Mbit memory that supports SPI up to 50 MHz.
 ;;
 ;;  Due to the nature of flash memory, writes can only change bits from 1 to
@@ -37,58 +37,58 @@
    #include "private.inc"
 
    ; Public Methods
-   global   M25P10.disableWrites
-   global   M25P10.enableWrites
-   global   M25P10.eraseAll
-   global   M25P10.eraseSector
-   global   M25P10.getId
-   global   M25P10.getStatus
-   global   M25P10.powerDown
-   global   M25P10.powerUp
-   global   M25P10.readByte
-   global   M25P10.readBytes
-   global   M25P10.setStatus
-   global   M25P10.writeByte
-   global   M25P10.writeBytes
+   global   M25P.disableWrites
+   global   M25P.enableWrites
+   global   M25P.eraseAll
+   global   M25P.eraseSector
+   global   M25P.getId
+   global   M25P.getStatus
+   global   M25P.powerDown
+   global   M25P.powerUp
+   global   M25P.readByte
+   global   M25P.readBytes
+   global   M25P.setStatus
+   global   M25P.writeByte
+   global   M25P.writeBytes
 
    ; Dependencies
    extern   SPI.io
-   extern   SPI.ioByte
-   extern   SPI.ioWord
    extern   Util.Frame
 
 
 
 ;; ---------------------------------------------------------------------------
-.m25p10                	code
+.m25p                	code
 ;; ---------------------------------------------------------------------------
 
 ;; ----------------------------------------------
-;;  void M25P10.disableWrites()
+;;  void M25P.disableWrites()
 ;;
 ;;  Clears the Write Enable Latch bit of the status register, prohibiting
 ;;  subsequent operations that write to the device.
 ;;
-M25P10.disableWrites:
+M25P.disableWrites:
    movlw    0x04
-   goto     SPI.ioByte
+   rcall    beginCommand
+   bra      endCommand
 
 
 
 ;; ----------------------------------------------
-;;  void M25P10.enableWrites()
+;;  void M25P.enableWrites()
 ;;
 ;;  Sets the Write Enable Latch bit of the status register, enabling write
 ;;  operations on the device.
 ;;
-M25P10.enableWrites:
+M25P.enableWrites:
    movlw    0x06
-   goto     SPI.ioByte
+   rcall    beginCommand
+   bra      endCommand
 
 
 
 ;; ----------------------------------------------
-;;  void M25P10.eraseAll()
+;;  void M25P.eraseAll()
 ;;
 ;;  Resets all memory locations to 0xff, unless one or both Block Protect bits
 ;;  (BP1, BP0) are set.  In that case, this method does nothing.
@@ -97,7 +97,7 @@ M25P10.enableWrites:
 ;;  complete.  This methods blocks until the Write In Progress (WIP) bit is
 ;;  reset to 0.
 ;;
-M25P10.eraseAll:
+M25P.eraseAll:
    movlw    0xc7
    rcall    beginCommand
    bra      endCommandConfirmWrite
@@ -105,16 +105,16 @@ M25P10.eraseAll:
 
 
 ;; ----------------------------------------------
-;;  void M25P10.eraseSector( frame[0..2] address )
+;;  void M25P.eraseSector( frame[0..2] address )
 ;;
 ;;  Sets all bits in the specified sector to 1.  Any address in the sector may
-;;  be used to indicate which one is to be cleared.  M25P10.enableWrites() must
+;;  be used to indicate which one is to be cleared.  M25P.enableWrites() must
 ;;  be called prior to this method.
 ;;
 ;;  Note that this is a slow operation, taking up to 3 seconds.  This method
 ;;  blocks until the write completes.
 ;;
-M25P10.eraseSector:
+M25P.eraseSector:
    movlw    0xd8
    rcall    beginCommandAddress
    bra      endCommandConfirmWrite
@@ -122,7 +122,7 @@ M25P10.eraseSector:
 
 
 ;; ----------------------------------------------
-;;  frame[0], frame[1..2] M25P10.getId()
+;;  frame[0], frame[1..2] M25P.getId()
 ;;
 ;;  Returns the 1-byte JEDEC manufacturer id (0x20 for STMicroelectronics) and
 ;;  the 2-byte device identification, which includes the memory type in the
@@ -130,10 +130,10 @@ M25P10.eraseSector:
 ;;  for the M25P10-A).
 ;;
 ;;  Note that this method returns 0 for all values unless the device has the
-;;  "X" process technology code.  See M25P10.powerUp() for an alternative
+;;  "X" process technology code.  See M25P.powerUp() for an alternative
 ;;  identification technique.
 ;;
-M25P10.getId:
+M25P.getId:
    movlw    0x9f
    rcall    beginCommand
 
@@ -150,7 +150,7 @@ M25P10.getId:
 
 
 ;; ----------------------------------------------
-;;  WREG M25P10.getStatus()
+;;  WREG M25P.getStatus()
 ;;
 ;;  Returns the current status byte, whose bits are organized as follows:
 ;;
@@ -161,20 +161,22 @@ M25P10.getId:
 ;;    ------X- WEL      ; Write Enable Latch
 ;;    -------X WIP      ; Write in Progress
 ;;
-M25P10.getStatus:
+M25P.getStatus:
    movlw    0x05
-   goto     SPI.ioByte
+   rcall    beginCommand
+   call     SPI.io
+   bra      endCommand
 
 
 
 ;; ----------------------------------------------
-;;  void M25P10.powerDown()
+;;  void M25P.powerDown()
 ;;
 ;;  Enters the extreme low-power consumption mode of the chip, typically
 ;;  about 5µA.  When in this mode, the device will not respond to any other
-;;  commands besides M25P10.powerUp().
+;;  commands besides M25P.powerUp().
 ;;
-M25P10.powerDown:
+M25P.powerDown:
    movlw    0xb9
    rcall    beginCommand
    bra      endCommand
@@ -182,7 +184,7 @@ M25P10.powerDown:
 
 
 ;; ----------------------------------------------
-;;  WREG M25P10.powerUp()
+;;  WREG M25P.powerUp()
 ;;
 ;;  Restores the chip to regular standby mode, drawing about 50µA.  If the
 ;;  device is in deep power-down mode, this method must be executed before
@@ -192,20 +194,20 @@ M25P10.powerDown:
 ;;  is 0x10 for the M25P10-A.  The device need not be in low-power mode to
 ;;  call this method, so the signature may be retrieved at any time.
 ;;
-M25P10.powerUp:
+M25P.powerUp:
    movlw    0xab
-   rcall    beginCommand
+   rcall    beginCommandAddress
    call     SPI.io
    bra      endCommand
 
 
 
 ;; ----------------------------------------------
-;;  WREG M25P10.readByte( frame[0..2] address )
+;;  WREG M25P.readByte( frame[0..2] address )
 ;;
 ;;  Returns the 8-bit value stored at the memory address specified.
 ;;
-M25P10.readByte:
+M25P.readByte:
    movlw    0x03
    rcall    beginCommandAddress
    call     SPI.io
@@ -214,18 +216,18 @@ M25P10.readByte:
 
 
 ;; ----------------------------------------------
-;;  void M25P10.readBytes( frame[0..2] address, frame[3] count, FSR0 buffer )
+;;  void M25P.readBytes( frame[0..2] address, frame[3] count, FSR0 buffer )
 ;;
 ;;  Reads up to a page of memory (256 bytes) and copies the data sequentially
 ;;  to the memory block whose base address is stored in FSR0.  The first
 ;;  address to be read doesn't actually have to be at a page boundary.  If the
 ;;  range extends past the end of physical memory, retrieval will resume at
-;;  location 0x00000000.  This differs from M25P10.writeBytes(), which always
+;;  location 0x00000000.  This differs from M25P.writeBytes(), which always
 ;;  works within the confines of a single page.
 ;;
 ;;  A count parameter of 0 indicates 256 bytes should be read.
 ;;
-M25P10.readBytes:
+M25P.readBytes:
    movlw    0x03
    rcall    beginCommandAddress
 
@@ -241,28 +243,30 @@ rdBytes:
 
 
 ;; ----------------------------------------------
-;;  void M25P10.setStatus( WREG status )
+;;  void M25P.setStatus( WREG status )
 ;;
-M25P10.setStatus:
+M25P.setStatus:
    movwf    Util.Frame + 1
    movlw    0x01
-   movwf    Util.Frame
-   call     SPI.ioWord
-   bra      waitForWriteComplete
+   rcall    beginCommand
+
+   movf     Util.Frame + 1, W
+   call     SPI.io
+   bra      endCommandConfirmWrite
 
 
 
 ;; ----------------------------------------------
-;;  void M25P10.writeByte( WREG value, frame[0..2] address )
+;;  void M25P.writeByte( WREG value, frame[0..2] address )
 ;;
 ;;  Performs a logical AND of the working register and the memory address
-;;  specified.  A call to M25P10.enableWrites() must preceed this operation.  A
+;;  specified.  A call to M25P.enableWrites() must preceed this operation.  A
 ;;  write attempt to a page protected by the Block Protect bits will be ig-
 ;;  nored.
 ;;
 ;;  This method blocks until the write is complete (typically 1.4 to 5 ms).
 ;;
-M25P10.writeByte:
+M25P.writeByte:
    movwf    Util.Frame + 3
    movlw    0x02
    rcall    beginCommandAddress
@@ -274,7 +278,7 @@ M25P10.writeByte:
 
 
 ;; ----------------------------------------------
-;;  void M25P10.writeBytes( frame[0..2] address, frame[3] count, FSR0 buffer )
+;;  void M25P.writeBytes( frame[0..2] address, frame[3] count, FSR0 buffer )
 ;;
 ;;  Performs a logical AND of the data memory pointed to by FSR0 and the Flash
 ;;  memory starting at the address specified, up to the parameterized count (a
@@ -288,7 +292,7 @@ M25P10.writeByte:
 ;;
 ;;  This method blocks until the write completes.
 ;;
-M25P10.writeBytes:
+M25P.writeBytes:
    movlw    0x02
    rcall    beginCommandAddress
 
@@ -358,21 +362,15 @@ endCommand:
 ;;
 endCommandConfirmWrite:
    bsf      PORTC, RC2
+
+   movlw    0xff
+waitBusy:
    nop
-   ; Fall through to next routine.
+   decfsz   WREG
+     bra    waitBusy
 
-
-
-;; ----------------------------------------------
-;;  void waitForWriteComplete()
-;;
-;;  Waits for the Write In Progress (WIP) flag to be cleared by the current
-;;  write command (if any).
-;;
-waitForWriteComplete:
-   ; Assert the chip select line and prepare to request status.
-   bcf      PORTC, RC2
    movlw    0x05
+   rcall    beginCommand
 
 waitChk:
    ; Check the WIP status bit.
@@ -381,8 +379,7 @@ waitChk:
      bra    waitChk                 ; no, keep waiting
 
    ; Write is complete, so de-assert the chip select line.
-   bsf      PORTC, RC2
-   return
+   bra      endCommand
 
 
 
