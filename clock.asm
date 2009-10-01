ccd7d8333daff4d6ef00091aad756dac83069744
Peter Heinrich <peter.heinrich@gmail.com>
2009-09-30 21:26:10 -0700

    Updated license to GPL v2 with Classpath exception

--- clock.asm
+++ clock.asm
@@ -1,42 +1,41 @@
 ;; ---------------------------------------------------------------------------
 ;;
 ;;  PIC Framework
+;;  Copyright (c) 2006,2008  Peter Heinrich
 ;;
-;;  Copyright (c) 2006-8  Peter Heinrich
-;;  All Rights Reserved
+;;  This program is free software; you can redistribute it and/or
+;;  modify it under the terms of the GNU General Public License
+;;  as published by the Free Software Foundation; either version 2
+;;  of the License, or (at your option) any later version.
 ;;
-;;  $URL$
-;;  $Revision$
+;;  Linking this library statically or dynamically with other modules
+;;  is making a combined work based on this library. Thus, the terms
+;;  and conditions of the GNU General Public License cover the whole
+;;  combination.
 ;;
-;;  Redistribution and use in source and binary forms, with or without
-;;  modification, are permitted provided that the following conditions are met:
-;;      * Redistributions of source code must retain the above copyright
-;;        notice, this list of conditions and the following disclaimer.
-;;      * Redistributions in binary form must reproduce the above copyright
-;;        notice, this list of conditions and the following disclaimer in the
-;;        documentation and/or other materials provided with the distribution.
-;;      * Neither the name of the PIC Modbus project nor the names of its
-;;        contributors may be used to endorse or promote products derived from
-;;        this software without specific prior written permission.
+;;  As a special exception, the copyright holders of this library give
+;;  you permission to link this library with independent modules to
+;;  produce an executable, regardless of the license terms of these
+;;  independent modules, and to copy and distribute the resulting
+;;  executable under terms of your choice, provided that you also meet,
+;;  for each linked independent module, the terms and conditions of the
+;;  license of that module. An independent module is a module which is
+;;  not derived from or based on this library. If you modify this
+;;  library, you may extend this exception to your version of the
+;;  library, but you are not obligated to do so. If you do not wish to
+;;  do so, delete this exception statement from your version.
 ;;
-;;  THIS SOFTWARE IS PROVIDED BY PETER HEINRICH ''AS IS'' AND ANY EXPRESS OR
-;;  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-;;  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
-;;  NO EVENT SHALL PETER HEINRICH BE LIABLE FOR ANY DIRECT, INDIRECT,
-;;  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
-;;  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES LOSS OF USE,
-;;  DATA, OR PROFITS OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
-;;  OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-;;  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-;;  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+;;  This program is distributed in the hope that it will be useful,
+;;  but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;  GNU General Public License for more details.
+;;
+;;  You should have received a copy of the GNU General Public License
+;;  along with this program; if not, write to the Free Software
+;;  Foundation, Inc., 51 Franklin Street, Boston, MA  02110-1301, USA.
 ;;
 ;; ---------------------------------------------------------------------------
-;;
 ;;  Provides a general-purpose wallclock with millisecond resolution.
-;;
-;; ---------------------------------------------------------------------------
-;;  $Author$
-;;  $Date$
 ;; ---------------------------------------------------------------------------
 
 
